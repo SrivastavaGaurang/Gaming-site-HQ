e22939241e45e09896d6a88df330304d0f8cc04c
Gaurang Srivastava <166457211+SrivastavaGaurang@users.noreply.github.com>
2025-03-30 03:49:05 +0530

    adding more games

--- server.js
+++ server.js
@@ -46,6 +46,70 @@ app.get('/challenges/challenge1', (req, res) => {
   app.get('/challenges/challenge6', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge6.html'));
   });
+  app.get('/challenges/challenge7', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge7.html'));
+  });
+  app.get('/challenges/challenge8', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge8.html'));
+  });
+  app.get('/challenges/challenge9', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge9.html'));
+  });
+  app.get('/challenges/challenge10', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge10.html'));
+  });
+  app.get('/challenges/challenge11', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge11.html'));
+  });
+  app.get('/challenges/challenge12', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge12.html'));
+  });
+  app.get('/challenges/challenge13', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge13.html'));
+  });
+  app.get('/challenges/challenge14', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge14.html'));
+  });
+  app.get('/challenges/challenge15', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge15.html'));
+  });
+  app.get('/challenges/challenge16', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge16.html'));
+  });
+  app.get('/challenges/challenge17', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge17.html'));
+  });
+  app.get('/challenges/challenge18', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge18.html'));
+  });
+  app.get('/challenges/challenge19', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge19.html'));
+  });
+  app.get('/challenges/challenge20', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge20.html'));
+  });
+  app.get('/challenges/challenge21', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge21.html'));
+  });
+  app.get('/challenges/challenge22', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge22.html'));
+  });
+  app.get('/challenges/challenge23', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge23.html'));
+  });
+  app.get('/challenges/challenge24', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge24.html'));
+  });
+  app.get('/challenges/challenge25', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge25.html'));
+  });
+  app.get('/challenges/challenge26', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge26.html'));
+  });
+  app.get('/challenges/challenge27', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'challenges', 'challenge27.html'));
+  });
+ 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
